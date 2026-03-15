73c4d6d422dfefa59a46991830cb94b22f48bcda
Vincent RAMPAL <vincent.rampal@gmail.com>
2026-03-15 16:41:26 +0100

    Change vid and pid to hide

--- boot-hide-and-lock.py
+++ boot-hide-and-lock.py
@@ -1,10 +1,27 @@
 import usb_cdc
 import usb_midi
 import storage
+import supervisor
 
-# Rename this file into boot.py to hide storage and REPL
-# Warning: it will become impossible to change code.py or boot.py after reboot
+# This must be run in boot.py and not code.py
 
+# These lines will change vendor id, device id, hide storage and REPL
+# WARNING: it will become IMPOSSIBLE to access or modify code.py or boot.py after reboot
+
+# Mimic a Logitech Unifying Receiver
+# see: https://devicehunt.com/view/type/usb/vendor/046D
+supervisor.set_usb_identification(
+    # Use an obsolete vid to not confuse Logitech drivers
+    # normal Logitech vid is 0x046D
+    vid=0x04E0,
+    pid=0xC52B,
+    manufacturer="Logitech, Inc.",
+    product="Unifying Receiver"
+)
+
+# Disable storage
 storage.disable_usb_drive()
+# Disable REPL
 usb_cdc.disable()
+# Disable MIDI
 usb_midi.disable()
